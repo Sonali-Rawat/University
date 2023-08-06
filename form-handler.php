7b1cb81a6c2d21add5ab5f1d6388c67d1674bf67
Sonali Rawat <106154741+Sonali-Rawat@users.noreply.github.com>
2023-08-06 21:17:43 +0530

    added remaining pages

---
+++ form-handler.php
@@ -0,0 +1,22 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+
+$email_from='info@easysharepro.com';
+$email_subject='New Form Submission';
+$email_body="User Name:$name.\n".
+            "User Email:$visitor_email.\n".
+            "Subject:$subject.\n".
+            "Message:$message .\n";
+
+            $to='sonalirawat7185@gmail.com';
+            $headers="From:$email_from \r\n";
+            $headers .="Reply-To:$visitor_email \r\n";
+            mail($to,$email_subject.$email_body,$headers);
+            header("Location: contact.html");
+            
+
+?>
